74aadf780963fffd8104a0e9a0aaafd50e789347
Danang Balany <55481761+danangbalany@users.noreply.github.com>
2022-06-15 11:11:07 +0700

    Update Bahasa_Mod.cs

--- ModTemplate/Harmony Patches/Bahasa_Mod.cs
+++ ModTemplate/Harmony Patches/Bahasa_Mod.cs
@@ -1,16 +1,1 @@
-﻿using BokuMono;
-using BokuMono.Data;
-using HarmonyLib;
-
-namespace ModTemplate.Harmony_Patches
-{
-    [HarmonyPatch(typeof(PlayerRecoveryMasterData), nameof(PlayerRecoveryMasterData.GetRecoveryRate))]
-    class Patch
-    {
-        static void Postfix(PlayerRecoveryMasterData __instance)
-        {
-            //This changes the time that the player wakes up to 6:00 AM, regardless of when they went to sleep. 
-            __instance.WakeUpTime = "6:00";
-        }
-    }
-}
+﻿
